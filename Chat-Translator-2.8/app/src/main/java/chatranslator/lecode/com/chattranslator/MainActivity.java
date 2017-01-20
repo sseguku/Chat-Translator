19f5c45a97ecbe1fca1b89257f32ac9ab4465353
Jingo Kisakye <sseguku@gmail.com>
2017-01-20 16:42:09 +0300

    Version 2.8

---
+++ Chat-Translator-2.8/app/src/main/java/chatranslator/lecode/com/chattranslator/MainActivity.java
@@ -0,0 +1,13 @@
+package chatranslator.lecode.com.chattranslator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
